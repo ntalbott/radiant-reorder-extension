24fa4bcc4048018cc5f5ded8fe5285fd7dd59c36
seancribbs <seancribbs@6226cccd-6db4-da11-80d6-0014220fd306>
2007-09-30 20:03:32 +0000

    Propagate test helper changes from r501 to extensions. Manage sass external with Piston.
    git-svn-id: http://svn.radiantcms.org/radiant/trunk/extensions/reorder@505 6226cccd-6db4-da11-80d6-0014220fd306

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,18 @@
-require File.dirname(__FILE__) + "/../../../../test/test_helper" unless defined? TEST_ROOT
+# Load the environment
+unless defined? RADIANT_ROOT
+  ENV["RAILS_ENV"] = "test"
+  require "#{File.expand_path(File.dirname(__FILE__) + "/../../../../")}/config/environment"
+end
+require "#{RADIANT_ROOT}/test/test_helper"
 
 class Test::Unit::TestCase
-  test_helper :extension_fixtures, :extension_tags
-  self.extension_fixture_path = File.dirname(__FILE__) + "/fixtures"
+  
+  # Include a helper to make testing Radius tags easier
+  test_helper :extension_tags
+  
+  # Add the fixture directory to the fixture path
+  self.fixture_path << File.dirname(__FILE__) + "/fixtures"
+  
+  # Add more helper methods to be used by all extension tests here...
+  
 end
